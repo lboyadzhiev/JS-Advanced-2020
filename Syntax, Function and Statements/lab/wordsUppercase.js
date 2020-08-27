349edf9e8c8969dbf85e7e6232886ff462cff0a4
lboyadzhiev <l.boyadzhievv@abv.bg>
2020-08-27 18:32:05 +0300

    restOfTasks

---
+++ Syntax, Function and Statements/lab/wordsUppercase.js
@@ -0,0 +1,13 @@
+/*
+    9. *Words Uppercase
+Write a program that extracts all words from a passed in string and converts them to upper case. The extracted words in upper case must be printed on a single line separated by ", ".
+The input comes as a single string argument - the text to extract and convert words from.
+The output should be a single line containing the converted string.
+*/
+
+function solve(input) {
+   return input.match(/\w+/g)
+      .map(x => x.toUpperCase()).join(', ');
+}
+
+console.log(solve('Hi, how are you?'))
