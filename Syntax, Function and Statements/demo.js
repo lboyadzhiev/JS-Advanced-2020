0d00bead17b60e1b2f592f1c61f185fb0715fde4
lboyadzhiev <l.boyadzhievv@abv.bg>
2020-08-27 16:16:40 +0300

    stringLength

---
+++ Syntax, Function and Statements/demo.js
@@ -0,0 +1,17 @@
+/*
+truthy
+
+When we say that a value is truthy in JS, what we mean is that the value coerces to true when evalueted in a boolean context 
+*/
+
+/*
+There are only six falsy values - false, null, undefined, NaN, 0 and "".
+*/
+
+/*
+Function - named list of instructons (statements and expressions)
+
+Can take parameters and return result
+   Functions names and parameters use camel case
+*/
+
---
+++ Syntax, Function and Statements/lab/stringLength.js
@@ -0,0 +1,27 @@
+/*
+    1. String Length
+Write a JS function that takes three string arguments as an input. 
+
+Calculate the sum of the length of the strings and the average length of the strings rounded down to the nearest integer.
+The input comes as three string arguments passed to your function.
+The output should be printed on the console on two lines.
+*/
+
+// function solve(x, y, z) {
+//    let sum = x.length + y.length + z.length;
+//    let avrg = Math.floor(sum / 3);
+
+//    console.log(sum);
+//    console.log(avrg);
+// }
+
+
+
+function solve(...params) {
+   let sum = params.reduce((a, b) => a + b.length, 0);
+   let avrg = Math.floor(sum / params.length);
+
+   return [sum, avrg].join('\n')
+}
+
+console.log(solve('chocolate', 'ice cream', 'cake'));
