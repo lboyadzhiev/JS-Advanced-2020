b509d85cd26893b63cb6595d85689b4503c3286c
lboyadzhiev <l.boyadzhievv@abv.bg>
2020-08-27 16:29:01 +0300

    mathOperations

---
+++ Syntax, Function and Statements/lab/mathOperations.js
@@ -0,0 +1,16 @@
+/*
+    2. Math Operations
+Write a JS function that takes two numbers and a string as an input. 
+
+The string may be one of the following: '+', '-', '*', '/', '%', '**'.  
+
+Print on the console the result of the mathematical operation between both numbers and the operator you receive as a string.
+The input comes as two numbers and a string argument passed to your function.
+The output should be printed on the console.
+*/
+
+function solve(x, y, operator) {
+   return eval(`${x}${operator}${y}`);
+}
+
+console.log(solve(5, 6, '+'));
