dbc8563bdd30c4708c893e83ce769485d1092a1a
lboyadzhiev <l.boyadzhievv@abv.bg>
2020-08-29 23:41:50 +0300

    calorieObject

---
+++ Syntax, Function and Statements/exercise/caloriesObject.js
@@ -0,0 +1,12 @@
+/*
+    5. Calorie Object
+Write a function that composes an object by given properties. The input comes as an array of strings. Every even index of the array represents the name of the food. Every odd index is a number that is equal to the calories in 100 grams of the given product. Assign each value to its corresponding property and print it on the console.
+The input comes as an array of string elements.
+The output should be printed on the console.
+*/
+
+function solve(input) {
+
+}
+
+solve(['Yoghurt', 48, 'Rise', 138, 'Apple', 52]);
