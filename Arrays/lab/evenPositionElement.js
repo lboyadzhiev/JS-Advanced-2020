f40c7a5581dce89936db45a12730fc53778a2532
lboyadzhiev <l.boyadzhievv@abv.bg>
2020-09-06 21:44:06 +0300

    evenPositionElements

--- Arrays/lab/evenPositionElement.js
+++ Arrays/lab/evenPositionElement.js
@@ -6,7 +6,12 @@ The output is the return value of your function. Collect all elements in a strin
 */
 
 function solve(input) {
+    let result = [];
+    for (let i = 0; i < input.length; i += 2) {
 
+        result.push(input[i]);
+    }
+    return result.join(' ');
 }
 
 console.log(solve(['20', '30', '40']));
--- Arrays/lab/sumFirstAndLast.js
+++ Arrays/lab/sumFirstAndLast.js
@@ -13,4 +13,4 @@ function solve(input) {
    return sum;
 }
 
-console.log(solve(['20']))
+    console.log(solve(['20', '30', '40']))
