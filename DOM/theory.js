31df2e8d65d2b71079fbcba351927e25d4331a7f
lboyadzhiev <l.boyadzhievv@abv.bg>
2020-10-02 09:47:08 +0300

    DOM Theory

---
+++ DOM/theory.js
@@ -0,0 +1,20 @@
+/*
+DOM is a standard of how to:
+   Get HTML element
+   Change HTML element
+   Add HTML element
+   Delete HTML element
+
+DOM Methods
+   DOM Methods - actions you can perform on HTML elements (line add or delete elements)
+   DOM Properties - values of HTML elements that you can set or change
+
+DOM Manipulations
+   Ways to find a certain HTML element in the DOM
+      By id - 'getElementById()',
+      By tag name - 'getElementsByTagName()',
+      By class name - 'getElementsByClassName()',
+      By CSS selector = 'querySelector()',
+*/
+
+
