956426b6656180ae20d1cde9ef44fc856b73fae9
lboyadzhiev <l.boyadzhievv@abv.bg>
2020-10-03 14:41:07 +0300

    DOM theory

--- DOM/theory.js
+++ DOM/theory.js
@@ -15,6 +15,37 @@ DOM Manipulations
       By tag name - 'getElementsByTagName()',
       By class name - 'getElementsByClassName()',
       By CSS selector = 'querySelector()',
+
+   The HTML DOM allows JavaScript to change the content of HTML elements
+      "innerHTML"
+      "attributes"
+      "setAttribute()"
+      "style.property"
+
+   We can create, append and remove HTML elements dynamically
+      "removeChild()"
+      "appendChild()"
+      "replaceChild()"
+      "document.write()"
+
+CSS Selectors 
+   They allow very fast and powerful element matching
+      "#main" - return the element with ID 'main'
+      "#content div" - select all <div>s inside #content
+      ".note, .alert" - all elements with class "note" ot "alert"
+      "input[name='login']" - <input> with name "login"
+
+Creating a new DOM element
+   Creating a new DOM element
+      let p = document.createElement("p");
+      let li = document.createElement("li");
+
+   Create a copy / cloning DOM element
+      let li = document.getElementById("my-list");
+      let newLli = li.cloneNode(true);
+
+      The above code creates a new elements. But these elements don't exist anywhere except as values insie variables
+
 */
 
 
