aa8baec684fb8a8ec334f221b6bba983429ef703
lboyadzhiev <l.boyadzhievv@abv.bg>
2020-10-07 10:50:17 +0300

    fillDropDown

--- DOM/excersice/05. JS-Advanced-DOM-Exercise-Resources/02. Fill-Dropdown/dropdown.js
+++ DOM/excersice/05. JS-Advanced-DOM-Exercise-Resources/02. Fill-Dropdown/dropdown.js
@@ -1,3 +1,20 @@
+/*
+Your task is to take values from input fields with ids "newItemText" and "newItemValue". Then you should create and append an <option> to the <select> with id "menu".
+
+
+*/
+
 function addItem() {
-    console.log('TODO:...');
+    let text = document.getElementById('newItemText');
+    let data = document.getElementById('newItemValue');
+
+    let select = document.getElementById('menu');
+    let option = document.createElement('option');
+    // let option = `<option value = ${data.value}>${text.value}</option>`;
+    option.value = data.value;
+    option.innerText = text.value;
+
+    select.appendChild(option);
+    text.value = '';
+    data.value = '';
 }
