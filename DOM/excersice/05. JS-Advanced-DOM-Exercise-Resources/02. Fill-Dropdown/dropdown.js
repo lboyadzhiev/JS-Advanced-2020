71de0d53a7e5a9d9b0031fafb93bba3478c80c22
lboyadzhiev <l.boyadzhievv@abv.bg>
2020-10-18 08:43:06 +0300

    dropDown

--- DOM/excersice/05. JS-Advanced-DOM-Exercise-Resources/02. Fill-Dropdown/dropdown.js
+++ DOM/excersice/05. JS-Advanced-DOM-Exercise-Resources/02. Fill-Dropdown/dropdown.js
@@ -7,9 +7,11 @@ Your task is to take values from input fields with ids "newItemText" and "newIte
 
 function addItem() {
 
-    //read references 
-    const text = document.querySelector('#newItemText').value;
-    const value = document.querySelector('#newItemValue').value;
+    //read references
+    const textInput =  document.querySelector('#newItemText');
+    const valueInput = document.querySelector('#newItemValue');
+    const text = textInput.value;
+    const value = valueInput.value;
 
     //create new HTML element
     const option = document.createElement('option');
@@ -21,6 +23,6 @@ function addItem() {
     document.querySelector('#menu').appendChild(option);
 
     // null the fields
-    document.querySelector('#newItemText').value = '';
-    document.querySelector('#newItemValue').value = '';
+    textInput.value = '';
+    valueInput.value = '';
 }
