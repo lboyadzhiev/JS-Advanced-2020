427846cd772f756d71db30b1fa430543786f9f1a
lboyadzhiev <l.boyadzhievv@abv.bg>
2020-10-25 15:15:01 +0200

    sortedLIst 100/100

--- Object And Classes/excersices/sortedList.js
+++ Object And Classes/excersices/sortedList.js
@@ -12,34 +12,38 @@ Your add and remove functions should return an class instance with the required 
 Submit the class definition as is, without wrapping it in any function.
 */
 
-class SortedList {
-   constructor(list = []) {
-       this.list = list.sort((a, b) =>  a - b);
-       this.size = this.list.length;
-   }
 
-   add(element) {
-       this.list.push(element);
-       this.list.sort((a, b) => a - b);
-       this.size++;
-       return;
-   }
 
-   remove(index) {
-       if (index < 0 || index >= this.list.length) {
-           throw new Error(`Index doesn't exist`);
-       } else {
-           this.list.splice(index, 1);
-           this.size--;
-           return;
-       }
-   }
+class List {
 
-   get(index) {
-       if (index < 0 || index >= this.list.length) {
-           throw new Error(`Index doesn't exist`);
-       } else {
-           return this.list[index];
-       }
-   }
+    constructor() {
+        this._list = [];
+        this.size = 0;
+    }
+
+    add(element) {
+        this._list.push(element);
+        this._list.sort((a, b) => a - b);
+        this.size++;
+    }
+
+    remove(index) {
+        if (index >= 0 && index < this._list.length) {
+            this._list.splice(index, 1);
+            this.size--;
+        }
+
+    }
+
+    get(index) {
+        return this._list[index];
+    }
 }
+
+let list = new List();
+list.add(5);
+list.add(6);
+list.add(7);
+console.log(list.get(1));
+list.remove(1);
+console.log(list.get(1));
