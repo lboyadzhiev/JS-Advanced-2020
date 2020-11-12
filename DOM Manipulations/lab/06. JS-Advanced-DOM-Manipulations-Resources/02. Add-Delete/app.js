13b3f9dad8cfe87e71e12271351106df0d53921e
lboyadzhiev <l.boyadzhievv@abv.bg>
2020-11-12 08:26:32 +0200

    add/delete

--- DOM Manipulations/lab/06. JS-Advanced-DOM-Manipulations-Resources/02. Add-Delete/app.js
+++ DOM Manipulations/lab/06. JS-Advanced-DOM-Manipulations-Resources/02. Add-Delete/app.js
@@ -1,3 +1,19 @@
 function addItem() {
-    //TODO...
+    let input = document.querySelector('#newText');
+
+    let li = document.createElement('li');
+    li.innerHTML = `${input.value} <a href='#'>[Delete]</a`;
+    input.value = '';
+
+    let ul = document.querySelector('#items');
+    ul.appendChild(li);
+
+    let link = document.querySelector('#delete');
+    li.addEventListener('click', deleteEelmenet);
+
+    function deleteEelmenet(e) {
+        let element = e.target.parentElement;
+
+        element.remove();
+    }
 }
