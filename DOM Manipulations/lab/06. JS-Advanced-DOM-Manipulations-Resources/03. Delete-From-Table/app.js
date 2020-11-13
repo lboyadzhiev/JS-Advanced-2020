2600be0d5d9a9599b6b222ad4c69b92d981c8c6d
lboyadzhiev <l.boyadzhievv@abv.bg>
2020-11-13 08:25:36 +0200

    deleteFromTable

--- DOM Manipulations/lab/06. JS-Advanced-DOM-Manipulations-Resources/03. Delete-From-Table/app.js
+++ DOM Manipulations/lab/06. JS-Advanced-DOM-Manipulations-Resources/03. Delete-From-Table/app.js
@@ -1,3 +1,16 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+   let inputEl = document.querySelector('input[name=email]');
+   let emailForDelete = inputEl.value;
+
+   let tdEmails = document.querySelectorAll('#customers td:nth-of-type(2)');
+
+   for(const email of tdEmails) {
+
+      if(email.textContent === emailForDelete) {
+         email.parentElement.remove();
+      } else {
+         let result = document.getElementById('result');
+         result.textContent = 'Not found.'
+      }
+   }
 }
