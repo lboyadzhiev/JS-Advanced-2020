212a4de0daccc896087d26079491bf40f84f6e30
lboyadzhiev <l.boyadzhievv@abv.bg>
2020-12-10 10:34:40 +0200

    start from the beginning

--- DOM Manipulations/06. JS-Advanced-DOM-Manipulations-Exercise-Resources/02. Notification/scripts.js
+++
@@ -1,9 +0,0 @@
-function notify(message) {
-    let notificationEl = document.getElementById('notification');
-    notificationEl.textContent = message;
-    notificationEl.style.display = 'block';
-
-    setTimeout(() => {
-        notificationEl.style.display = 'none';
-    }, 2000);
-}
